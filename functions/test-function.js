5d772a4f86834459776abc1006565eac511a902d
rdsteam18 <149048516+rdsteam18@users.noreply.github.com>
2025-04-22 19:52:08 +0530

    Update test-function.js

--- functions/test-function.js
+++ functions/test-function.js
@@ -1,10 +1,10 @@
 exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Test function works!",
-            githubTokenSet: !!process.env.GITHUB_TOKEN,
-            event: event
-        })
-    };
-}
+         return {
+             statusCode: 200,
+             body: JSON.stringify({
+                 message: "Test function works!",
+                 githubTokenSet: !!process.env.GITHUB_TOKEN,
+                 event: event
+             })
+         };
+     }
